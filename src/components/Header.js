54a4332b84628925ff2dba46ee72353c11ff647d
Sanchez <ingabuga@yandex.ru>
2022-09-19 20:26:05 +0700

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo-mesto_white.svg';
+
+function Header() {
+    return (
+        <header class="header">
+            <img className="logo header__logo" alt="Логотип" src={logo}  />
+        </header>
+    )
+}
+
+export default Header;
