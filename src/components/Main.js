a2f990f31cb6e96e593aa44387477d8bfd44085e
Sanchez <ingabuga@yandex.ru>
2022-09-29 19:07:24 +0700

    add imperative method

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,37 @@
 import React from 'react';
 
 function Main() {
+
+function handleEditAvatarClick() {
+    const popupAvatar = document.querySelector('.popup_avatar');
+    popupAvatar.classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+    const popupProfile = document.querySelector('.popup_profile');
+    popupProfile.classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+    const popupProfile = document.querySelector('.popup_place');
+    popupProfile.classList.add('popup_opened');
+}
+
     return(
         <main>
             <section className="profile">
                 <div className="profile__info">
                     <div className="profile__avatar-frame">
-                        <div className="profile__avatar-hover"></div>
+                        <div className="profile__avatar-hover" onClick={handleEditAvatarClick}></div>
                         <img className="profile__avatar" src="#" name="avatar" alt="Улыбающийся Жак Ив Кусто в красной шапочке" />
                      </div>  
                     <div className="profile__text">
                         <h1 className="profile__title">Жак Ив Кусто</h1>
-                        <button aria-label="Редактирование профиля" className="profile__edit-button" type="button"></button>
+                        <button aria-label="Редактирование профиля" className="profile__edit-button" type="button" onClick={handleEditProfileClick}></button>
                         <p className="profile__title-job">Исследователь океана</p>
                     </div>  
                 </div>
-                <button aria-label="Добавление карточки" className="profile__add-button" type="button"></button>
+                <button aria-label="Добавление карточки" className="profile__add-button" type="button" onClick={handleAddPlaceClick}></button>
             </section>
             <section className="elements">
                 <ul className="elements__element"></ul>
