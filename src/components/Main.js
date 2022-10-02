8aa86e4ec79a54d3adda2ca1a7e774a503ba8e92
Sanchez <ingabuga@yandex.ru>
2022-10-02 18:58:16 +0600

    add api name profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import api from '../utils/Api.js';
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar}) {
+    const [userName, setUserName] = useState('');
+    const [userDescription, setUserDescription] = useState('');
+    const [userAvatar, setUserAvatar] = useState('');
+    // const [userId, setUserId] = useState('');
 
 // function handleEditAvatarClick() {
 //     const popupAvatar = document.querySelector('.popup_avatar');
@@ -17,6 +22,22 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar}) {
 //     popupProfile.classList.add('popup_opened');
 // }
 
+useEffect(() => {
+    Promise.all([api.getUserData()])
+        .then(allData => {
+            const [userData] = allData;
+            return [userData]
+        })
+        .then(([userData]) => {
+            setUserName(userData.name);
+            setUserDescription(userData.about);
+            setUserAvatar(userData.avatar);
+        })
+        .catch(err => console.log(`Не удалось загрузить данные. Ошибка: ${err}`));
+        
+})
+
+
     return(
         <main>
             <section className="profile">
@@ -26,16 +47,22 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar}) {
                         <img className="profile__avatar" src="#" name="avatar" alt="Улыбающийся Жак Ив Кусто в красной шапочке" />
                      </div>  
                     <div className="profile__text">
-                        <h1 className="profile__title">Жак Ив Кусто</h1>
+                        <h1 className="profile__title">
+                            {/* Жак Ив Кусто */}
+                            {userName}
+                            </h1>
                         <button aria-label="Редактирование профиля" className="profile__edit-button" type="button" onClick={onEditProfile}></button>
-                        <p className="profile__title-job">Исследователь океана</p>
+                        <p className="profile__title-job">
+                            {/* Исследователь океана */}
+                            {userDescription}
+                            </p>
                     </div>  
                 </div>
                 <button aria-label="Добавление карточки" className="profile__add-button" type="button" onClick={onAddPlace}></button>
             </section>
-            <section className="elements">
+            {/* <section className="elements">
                 <ul className="elements__element"></ul>
-            </section>
+            </section> */}
         </main>
     )
 }
---
+++ src/utils/Api.js
@@ -0,0 +1,109 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+
+  getUserData() {
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: 'GET',
+        headers: this._headers
+      })
+        .then(res => this._check(res))
+  }
+
+  getInitialCards() {
+      return fetch(`${this._baseUrl}/cards`, {
+        method: 'GET',
+        headers: this._headers
+      })
+        .then(res => this._check(res))
+  }
+
+
+  patchUserData(name, about) {
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: name,
+          about: about
+        })
+      })
+        .then(res => this._check(res))
+        
+  }
+
+  patchAvatar(link) {
+      return fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          avatar: link
+        })
+      })
+        .then(res => this._check(res))
+  }
+
+
+  addNewCard(name, link) {
+      return fetch(`${this._baseUrl}/cards`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: name,
+          link: link
+        })
+      })
+        .then(res => this._check(res))
+  }
+
+  removeCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(res => this._check(res))
+  }
+  
+
+  addLike(id) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+      .then(res => this._check(res))
+  }
+
+  removeLike(id) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(res => this._check(res))
+  }
+
+    
+
+  _check(res) {
+      if (res.ok) {
+        return res.json();
+      }
+  
+      return Promise.reject(res);
+  }
+  
+}  
+
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-49',
+  headers: {
+      authorization: 'ccf50a2a-0bfe-45bb-ba00-99b5825eb2e5',
+      'Content-Type': 'application/json'
+  }
+});
+
+export default api;
+
