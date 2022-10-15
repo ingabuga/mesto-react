6e4bf748ab9e7a61161b842939f811732fcc7100
Sanchez <ingabuga@yandex.ru>
2022-10-15 23:09:37 +0600

    add few changes

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,28 +1,42 @@
-import { useEffect, useRef} from 'react';
+import { useEffect, useRef, useState} from 'react';
 import PopupWithForm from './PopupWithForm.js';
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
-    const inputRef = useRef();
-    // let inputAvatar = inputRef.current.value;
+    // const inputRef = useRef();
 
-    function handleSubmit(evt) {
-        evt.preventDefault();
+    // function handleSubmit(evt) {
+    //     evt.preventDefault();
         
-        onUpdateAvatar({
-            avatar: inputRef.current.value
-        });
+    //     onUpdateAvatar({
+    //         avatar: inputRef.current.value
+    //     });
+    // }
+
+
+    // useEffect(() =>{
+    //     inputRef.current.value = '';
+    // }, [isOpen])
+
+    const [avatar, setAvatar] = useState("");
+    const currentAvatar = useRef(null);
+  
+    function handleChange(e) {
+      setAvatar(e.target.value);
     }
-
-
-    useEffect(() =>{
-        inputRef.current.value = '';
-    }, [isOpen])
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      onUpdateAvatar({
+        avatar: currentAvatar.current,
+      });
+      setAvatar("");
+    }
 
     return (
 
     <PopupWithForm name="avatar" title="Обновить аватар" isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit} buttonText="Сохранить">
             <div className="popup__field">
-                <input  type="url" id="avatar-input" placeholder="Ссылка на аватар" name="link" className="popup__text popup__text_input_job" required ref={inputRef}/>
+                <input  value={avatar} type="url" id="avatar-input" placeholder="Ссылка на аватар" name="link" className="popup__text popup__text_input_job" required ref={currentAvatar} onChange={handleChange}/>
                 <span id="avatar-input-error" className="error"></span>
             </div>
     </PopupWithForm>
